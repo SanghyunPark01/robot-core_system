304cd5871b49d2ea9b1253bc3123840a3d09e7af
SanghyunPark01 <pash0302@naver.com>
2023-01-29 03:36:12 +0900

    edit

--- src/timer_node.cpp
+++ src/timer_node.cpp
@@ -1,6 +1,7 @@
 /*완료 - 테스트 성공
 --------------------------------------------------------
 Sub. 도킹완료 여부
+Sub. Big_mode
 
 Pub. 타이머 플래그 : 5분 주행 / 30초 충전 (2: 5분완료(nav mode & docking mode), 1: 30초 완료(docking mode), 0: 미완료)
 - to small_mode_setter_node
@@ -15,19 +16,30 @@ Pub. 타이머 플래그 : 5분 주행 / 30초 충전 (2: 5분완료(nav mode & 
 
 int nTimer_mode = 2; //2: 주행중 , 1: 도킹완료 후
 int count = 0;
+int nBigMode = 1;
 class NodeServer
 {
 private:
     ros::NodeHandle _nh;
     ros::Publisher _pub_timer_flag;
     ros::Subscriber _sub_docking_finish_flag;
+    ros::Subscriber _sub_big_mode;
     int _nDockingFinish_Flag = 0;
 public:
     NodeServer(){
         _pub_timer_flag = _nh.advertise<std_msgs::Int32>("/Timer_flag",1);
+
+        _sub_big_mode = _nh.subscribe("/Big_mode", 10, &NodeServer::callback_Big_mode, this); // 1: full automatic, 0: only docking
         _sub_docking_finish_flag = _nh.subscribe("/docking_finish_flag", 10, &NodeServer::callback_finish_docking, this); //
     }
     void callback_finish_docking(const std_msgs::Int32 &value);
+    void callback_Big_mode(const std_msgs::Int32 &value){
+        if(value.data == 0){
+            nBigMode = 0;
+        }else if(value.data == 1){
+            nBigMode = 1;
+        }
+    }
     void publish(int pub){
         std_msgs::Int32 msg;
         msg.data = pub;
@@ -55,6 +67,9 @@ int main(int argc, char **argv)
     NodeServer subpub;
     ros::Rate loop_rate(1000);
     while(ros::ok()){
+        if(nBigMode == 0){
+            count = 0;
+        }
         if(nTimer_mode == 2 && (count > Mode2_sec)){
             subpub.publish(2);
         }else if(nTimer_mode == 1 && (count > Mode1_sec)){
